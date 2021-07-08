af7877da6aa8d66f04bb8d8ea2b8a07d58c8866f
lekau <lekau9222@gmail.com>
2021-07-08 10:01:48 +0200

    forgot to push the thing

--- api_user.py
+++ api_user.py
@@ -2,7 +2,7 @@ import requests
 
 BASE = "http://127.0.0.1:5000"
 
-response = requests.get(BASE + "/hello")
+response = requests.get(BASE + "/episode/<int:id>")
 responses = requests.post(BASE + "/hello")
 print(response.json())
 print(responses.json())
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__) #Create Flask object app
@@ -7,14 +7,21 @@ api = Api(app) #creating api object wrapping app as API
 
 episode_args = reqparse.RequestParser() #creating arguments for the episode 
 #adding required arguments 
-episode_args.add_argument(name="id", type=int,help="Episode's id", required=True)
 episode_args.add_argument(name="name", type=str, help="name of episode", required=True)
 episode_args.add_argument(name="listens", type=int, help="Number of listens")
 episode_args.add_argument(name="likes", type=int, help="Number of likes")
 
 
 
-episodes = {}
+episodes = {1:{"episode_name":"How to build a car","discription":"This is an episode on how to build your own car using boxes and glue", "listens":15, "likes":7},
+            2:{"episode_name":"How to build a car","discription":"This is an episode on how to build your own car using boxes and glue", "listens":145, "likes":7},
+            3:{"episode_name":"How to build a car","discription":"This is an episode on how to build your own car using boxes and glue", "listens":59, "likes":7},
+            4:{"episode_name":"How to build a car","discription":"This is an episode on how to build your own car using boxes and glue", "listens":1500, "likes":7},
+            5:{"episode_name":"How to build a car","discription":"This is an episode on how to build your own car using boxes and glue", "listens":515, "likes":7},
+            6:{"episode_name":"How to build a car","discription":"This is an episode on how to build your own car using boxes and glue", "listens":786, "likes":7},
+            7:{"episode_name":"How to build a car","discription":"This is an episode on how to build your own car using boxes and glue", "listens":2021, "likes":7},
+            8:{"episode_name":"my cats got married","discription":"This is an episode on how to build your own car using boxes and glue", "listens":49, "likes":7},
+            9:{"episode_name":"How to build an API","discription":"This is an episode on how to build your own car using boxes and glue", "listens":11456, "likes":7}}
 class episode(Resource): #Create class episode inheriting from Resource class to handle episode requests
     def get(self, episode_id):
         """"
@@ -30,7 +37,7 @@ api.add_resource(episode, "/episode/<int:id>") #adding episode class to resource
 
 @app.route('/')
 def home():
-   return "Welcome to my Podcast World!"
+    return render_template('index.html', episodes = episodes)
 
 
 if __name__ == "__main__":
